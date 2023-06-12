bdaea3de08ba1959de48560c3eb1596b7b83593b
DaviD <DaviD@192.168.56.1>
2023-06-12 12:53:37 +0200

    Segundo commit

--- Git/src/Paquete1/Principal.java
+++ Git/src/Paquete1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		System.out.println("Haciendo control de versiones en Git");
+		System.out.println("Prueba para realizar un segundo commit");
 
 	}
 
